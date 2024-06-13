b530238416c75ea5c97ec9c4759a79ea0be5904c
Dimitris-X <154934003+Dimitris-X@users.noreply.github.com>
2024-06-13 19:19:51 +0300

    Update Planet.py

--- Planet.py
+++ Planet.py
@@ -10,16 +10,26 @@ class Planet:
         self.mass = mass
         self.pos = position
         self.vel = velocity
-        self.acc = numpy.array([0,0])
+        self.acc = np.array([0,0])
 
     def return_particle(self):
         return rebound.Particle(m=self.mass, x=self.pos[0], y=self.pos[1], vx=self.vel[0], vy=self.vel[1])
 
     def return_vector(self, type):
         match type:
-            case "1":
+            case "pos_vel":
                 return np.concatenate(self.pos, self.vel)
-            case
+            case "pos_vel_acc":
+                return np.concatenate(self.pos, self.vel, self.acc)
 
+    def __repr__(self):
+        return f"pos={self.pos}, vel={self.vel}"
 
+    def __str__(self):
+        return f"pos={self.pos}, vel={self.vel}"
+
+    def copy(self):
+        new_planet = Planet(self.mass, self.pos.copy(), self.vel.copy())
+        new_planet.acc = self.acc.copy()
+        return new_planet
 
